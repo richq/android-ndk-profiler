124b662216c7f207a1d4f059eae41e1b427e6b21
Richard Quirk <richard.quirk@gmail.com>
2011-06-21 21:37:26 +0000

    Use includable makefile snippet
    git-svn-id: https://android-ndk-profiler.googlecode.com/svn/trunk@32 f564a05a-c8e1-f27b-dda1-4d4f22e13d0d

--- example/timing-test/jni/Android.mk
+++ example/timing-test/jni/Android.mk
@@ -1,15 +1,5 @@
 LOCAL_PATH := $(call my-dir)
-# remove -ffunction-sections and -fomit-frame-pointer from the default compile flags
-TARGET_thumb_release_CFLAGS := $(filter-out -ffunction-sections,$(TARGET_thumb_release_CFLAGS))
-TARGET_thumb_release_CFLAGS := $(filter-out -fomit-frame-pointer,$(TARGET_thumb_release_CFLAGS))
-TARGET_CFLAGS := $(filter-out -ffunction-sections,$(TARGET_CFLAGS))
-
-# include libandprof.a in the build
-include $(CLEAR_VARS)
-LOCAL_MODULE := andprof
-LOCAL_SRC_FILES := $(TARGET_ARCH_ABI)/libandprof.a
-include $(PREBUILT_STATIC_LIBRARY)
-
+include $(LOCAL_PATH)/../../../android-ndk-profiler.mk
 
 include $(CLEAR_VARS)
 
