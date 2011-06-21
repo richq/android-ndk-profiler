550cc31b58b0efd3406893fe8786bc1de9759679
Richard Quirk <richard.quirk@gmail.com>
2011-06-21 21:35:04 +0000

    add includable makefile snippet
    git-svn-id: https://android-ndk-profiler.googlecode.com/svn/trunk@31 f564a05a-c8e1-f27b-dda1-4d4f22e13d0d

---
+++ android-ndk-profiler.mk
@@ -0,0 +1,9 @@
+TARGET_thumb_release_CFLAGS := $(filter-out -ffunction-sections,$(TARGET_thumb_release_CFLAGS))
+TARGET_thumb_release_CFLAGS := $(filter-out -fomit-frame-pointer,$(TARGET_thumb_release_CFLAGS))
+TARGET_CFLAGS := $(filter-out -ffunction-sections,$(TARGET_CFLAGS))
+
+# include libandprof.a in the build
+include $(CLEAR_VARS)
+LOCAL_MODULE := andprof
+LOCAL_SRC_FILES := $(TARGET_ARCH_ABI)/libandprof.a
+include $(PREBUILT_STATIC_LIBRARY)
