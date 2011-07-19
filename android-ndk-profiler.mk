1a879cb39c22c3072cc9ef54d7474af49174927d
Richard Quirk <richard.quirk@gmail.com>
2011-07-19 17:24:34 +0000

    Removed omit-frame-pointer, etc, from ARM flags
    git-svn-id: https://android-ndk-profiler.googlecode.com/svn/trunk@42 f564a05a-c8e1-f27b-dda1-4d4f22e13d0d

--- android-ndk-profiler.mk
+++ android-ndk-profiler.mk
@@ -1,5 +1,7 @@
 TARGET_thumb_release_CFLAGS := $(filter-out -ffunction-sections,$(TARGET_thumb_release_CFLAGS))
 TARGET_thumb_release_CFLAGS := $(filter-out -fomit-frame-pointer,$(TARGET_thumb_release_CFLAGS))
+TARGET_arm_release_CFLAGS := $(filter-out -ffunction-sections,$(TARGET_arm_release_CFLAGS))
+TARGET_arm_release_CFLAGS := $(filter-out -fomit-frame-pointer,$(TARGET_arm_release_CFLAGS))
 TARGET_CFLAGS := $(filter-out -ffunction-sections,$(TARGET_CFLAGS))
 
 # include libandprof.a in the build
