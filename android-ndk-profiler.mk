aa07062bd9396617518e01c4a319127ab0f9db7d
Richard Quirk <richard.quirk@gmail.com>
2013-01-11 21:31:31 +0100

    Add -llog to the exported (required) libraries

--- android-ndk-profiler.mk
+++ android-ndk-profiler.mk
@@ -11,4 +11,5 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := android-ndk-profiler
 LOCAL_SRC_FILES := $(TARGET_ARCH_ABI)/libandroid-ndk-profiler.a
 LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)
+LOCAL_EXPORT_LDLIBS := -llog
 include $(PREBUILT_STATIC_LIBRARY)
