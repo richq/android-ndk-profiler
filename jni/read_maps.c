5cba39c8e514fa0a2d76d76e3eb1b4619caa17ec
Richard Quirk <richard.quirk@gmail.com>
2014-07-27 15:16:29 +0200

    Whitespace cleanups

--- jni/read_maps.c
+++ jni/read_maps.c
@@ -21,13 +21,13 @@ extern int opt_is_shared_lib;
 
 void free_maps(struct proc_map *s)
 {
-    struct proc_map *next = s->next;
-    while (next != NULL) {
-        struct proc_map *tmp = next;
-        next = next->next;
-        free(tmp);
-    }
-    free(s);
+	struct proc_map *next = s->next;
+	while (next != NULL) {
+		struct proc_map *tmp = next;
+		next = next->next;
+		free(tmp);
+	}
+	free(s);
 }
 
 struct proc_map *read_maps(FILE *fp, const char *lname)
@@ -36,48 +36,37 @@ struct proc_map *read_maps(FILE *fp, const char *lname)
 	struct proc_map *current = NULL;
 	size_t namelen = strlen(lname);
 
-	while (fgets(s_line, sizeof(s_line), fp) != NULL) 
-	{
+	while (fgets(s_line, sizeof(s_line), fp) != NULL) {
 		size_t len = strlen(s_line);
 		len--;
 		s_line[len] = 0;
 
-
-		if (namelen < len && strcmp(lname, &s_line[len - namelen]) == 0) {
+		if (namelen < len
+		    && strcmp(lname, &s_line[len - namelen]) == 0) {
 			char c[1];
 			char perm[4];
 			int lo, base, hi;
 			sscanf(s_line, "%x-%x %4c %x %c", &lo, &hi, perm, &base, c);
 
-			if (results == NULL) 
-			{
+			if (results == NULL) {
 				current = malloc(sizeof(struct proc_map));
-				if (!current)
-				{
+				if (!current) {
 					LOGI("error allocating memory");
 					return NULL;
 				}
 				current->next = NULL;
 				results = current;
-			} 
-			else 
-			{
+			} else {
 				current->next = malloc(sizeof(struct proc_map));
 				current = current->next;
-				if (!current)
-				{
+				if (!current) {
 					LOGI("error allocating memory");
 					return NULL;
 				}
 				current->next = NULL;
 			}
 
-			LOGI(	"process '%s', base = 0x%x, lo = 0x%x, hi = 0x%x"
-			, 	lname
-			, 	base
-			, 	lo
-			, 	hi
-			);
+			LOGI("process '%s', base = 0x%x, lo = 0x%x, hi = 0x%x", lname, base, lo, hi);
 
 			current->base = base;
 			current->lo = lo;
@@ -87,19 +76,18 @@ struct proc_map *read_maps(FILE *fp, const char *lname)
 	return results;
 }
 
-unsigned int get_real_address(const struct proc_map *maps, unsigned int fake)
+unsigned int get_real_address(const struct proc_map *maps,
+			      unsigned int fake)
 {
-    const struct proc_map *mp = maps;
-    while (mp) {
-        if (fake >= mp->lo && fake <= mp->hi) 
-	{
-		if (opt_is_shared_lib)
-		{
-           		return fake - mp->lo;
+	const struct proc_map *mp = maps;
+	while (mp) {
+		if (fake >= mp->lo && fake <= mp->hi) {
+			if (opt_is_shared_lib) {
+				return fake - mp->lo;
+			}
+			return fake;
 		}
-		return fake;
-        }
-        mp = mp->next;
-    }
-    return fake;
+		mp = mp->next;
+	}
+	return fake;
 }
