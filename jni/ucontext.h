b22514a66c0477dd34eadf7039e404bfc6f38c1b
Richard Quirk <richard.quirk@gmail.com>
2015-04-05 08:29:37 +0200

    Remove ucontext from jni/ucontext.h
    The struct ucontext is now available in system headers starting with NDK
    r10d, so there's no need to copy it here.

--- jni/ucontext.h
+++ jni/ucontext.h
@@ -13,12 +13,4 @@
 #include <asm/sigcontext.h>       /* for sigcontext */
 #include <asm/signal.h>           /* for stack_t */
 
-typedef struct ucontext {
-	unsigned long uc_flags;
-	struct ucontext *uc_link;
-	stack_t uc_stack;
-	struct sigcontext uc_mcontext;
-	unsigned long uc_sigmask;
-} ucontext_t;
-
 #endif
